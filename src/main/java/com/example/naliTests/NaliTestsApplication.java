ae80c7a7906be0e2612185cbe3724830ccfdfa9c
«MaxRubshik» <rubshikmax@gmail.com>
2020-10-26 10:13:16 +0300

    start

---
+++ src/main/java/com/example/naliTests/NaliTestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.naliTests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaliTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaliTestsApplication.class, args);
+	}
+
+}
